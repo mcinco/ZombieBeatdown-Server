6e1c608c97b70e9cc94e62409d330bcf77112686
Micah Cinco <micahcinco@gmail.com>
2014-08-29 05:54:38 +1200

    Updating code - 29/08

--- zb-server/servertest.py
+++ zb-server/servertest.py
@@ -1,78 +1,76 @@
-#!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from os import curdir, sep
-import cgi, task
+import cgi, json
+from task import Task
 urls = []
 
 PORT_NUMBER = 8080
 
-#This class will handles any incoming request from
-#the browser 
+# This class will handles any incoming request from
+# the browser 
 class myHandler(BaseHTTPRequestHandler):
 	
-	#Handler for the GET requests
+	# Handler for the GET requests
 	def do_GET(self):
-		if self.path=="/":
-			self.path="/server.html"
+		if self.path == "/":
+			self.path = "/server.html"
 
 		try:
-			#Check the file extension required and
-			#set the right mime type
+			# Check the file extension required and
+			# set the right mime type
 
 			sendReply = False
 			if self.path.endswith(".html"):
-				mimetype='text/html'
+				mimetype = 'text/html'
 				sendReply = True
 			if self.path.endswith(".js"):
-				mimetype='application/javascript'
+				mimetype = 'application/javascript'
 				sendReply = True
 			if self.path.endswith(".css"):
-				mimetype='text/css'
+				mimetype = 'text/css'
 				sendReply = True
 
 			if sendReply == True:
-				#Open the static file requested and send it
+				# Open the static file requested and send it
 				f = open(curdir + sep + self.path) 
 				self.send_response(200)
-				self.send_header('Content-type',mimetype)
+				self.send_header('Content-type', mimetype)
 				self.end_headers()
 				self.wfile.write(f.read())
 				f.close()
 			return
 
 		except IOError:
-			self.send_error(404,'File Not Found: %s' % self.path)
+			self.send_error(404, 'File Not Found: %s' % self.path)
 
-	#Handler for the POST requests
+	# Handler for the POST requests
 	def do_POST(self):
-		if self.path=="/send":
+		if self.path == "/send":
 			form = cgi.FieldStorage(
-				fp=self.rfile, 
+				fp=self.rfile,
 				headers=self.headers,
 				environ={'REQUEST_METHOD':'POST',
 		                 'CONTENT_TYPE':self.headers['Content-Type'],
 			})
 
-			print "URL(s): %s" % form["urls"].value
-			print "Priority: %s" % form["priority"].value
-			self.send_response(200)
-			self.end_headers()
+# 			print "URL(s): %s" % form["urls"].value
+# 			print "Priority: %s" % form["priority"].value
 			urls = form.getvalue("urls").split(",")
-			print urls
-			print len(urls)
-			#task = Task make_task(urls, form.getvalue("priority"))
-			self.wfile.write("URL(s): %s \n" % form["urls"].value)
-			self.wfile.write("Priority: %s \n" % form["priority"].value)
+			t = Task(urls, form.getvalue("priority"))
+			print t.printTask()
+			self.wfile.write(t.printTask())
+			self.send_response(200)
+			self.end_headers()		
 			return			
 			
 			
 try:
-	#Create a web server and define the handler to manage the
-	#incoming request
+	# Create a web server and define the handler to manage the
+	# incoming request
 	server = HTTPServer(('', PORT_NUMBER), myHandler)
 	print 'Started httpserver on port ' , PORT_NUMBER
 	
-	#Wait forever for incoming http requests
+	# Wait forever for incoming http requests
 	server.serve_forever()
 
 except KeyboardInterrupt:
--- zb-server/task.py
+++ zb-server/task.py
@@ -1,3 +1,5 @@
+import json
+
 class Task(object):
     urls = []
     priority = ""
@@ -7,7 +9,27 @@ class Task(object):
         self.urls = urls
         self.priority = priority
         self.timeout = 100
+        self.isCompleted = False
 
-def make_task(urls, priority, timeout):
-    task = Task(urls, priority, timeout)
-    return task
+    def getURLS(self):
+        return self.urls
+        
+    def getPriority(self):
+        return self.priority
+        
+    def getTimeout(self):
+        return self.timeout
+        
+    def getSize(self):
+        return len(self.urls)
+        
+    def getStatus(self):
+        if self.isCompleted == False:
+            return "Not Completed"
+        elif self.isCompleted == True:
+            return "Completed"
+        
+    def printTask(self):
+        return json.dumps(self.__dict__)
+        #return "URL(s): %s\n" % self.getURLS(), "Priority: %s\n" % self.getPriority(), '\nTimeout: ', self.getTimeout(), '\nSize: ', self.getSize(), '\nStatus: ', self.getStatus()
+        
