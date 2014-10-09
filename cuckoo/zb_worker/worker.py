1b6142be07b3035de5cbc0d5f79f4b5962e19659
Micah Cinco <micahcinco@gmail.com>
2014-10-10 00:16:24 +1300

    Cleaning up code

--- cuckoo/zb_worker/worker.py
+++ cuckoo/zb_worker/worker.py
@@ -8,8 +8,6 @@ tid_list=[]
 def start_worker():
     task = mongo.pull_task()
     if task != None:
-        #print json.dumps(task, sort_keys=True, indent=4, default=json_util.default)
-        #print task['_id']
         global tid_list 
         tid_list = taskhandler.add_urls(task)
         print tid_list
@@ -21,7 +19,6 @@ def check_status(_id):
     try:
         while True:
             time.sleep(5)
-#             print tid_list
             task_status = taskhandler.task_done(tid_list)
 
             if task_status == False:
@@ -30,7 +27,6 @@ def check_status(_id):
                 print 'All URLs done - Task is completed!\n'
                 mongo.task_done(_id)
                 break
-            
     except KeyboardInterrupt:
         return
 
